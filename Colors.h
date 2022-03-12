c5688db301b34cebb6aff86aaec2667007eb3ae1
Ag <theag768@gmail.com>
2022-03-12 17:01:00 +0545

    added validation

--- Colors.h
+++ Colors.h
@@ -14,67 +14,67 @@ HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
  *             6 = Yellow      E = Light Yellow
  *             7 = White       F = Bright White
  */
-void black()
+inline void black()
 {
     SetConsoleTextAttribute(h,0x00 | 0xf8);
 }
-void darkblue()
+inline void darkblue()
 {
     SetConsoleTextAttribute(h,0x01 | 0xf8);
 }
-void darkgreen()
+inline void darkgreen()
 {
     SetConsoleTextAttribute(h,0x02 | 0xf8);
 }
-void darkcyan()
+inline void darkcyan()
 {
     SetConsoleTextAttribute(h,0x03 | 0xf8);
 }
-void darkred()
+inline void darkred()
 {
     SetConsoleTextAttribute(h,0x04 | 0xf8);
 }
-void darkpurple()
+inline void darkpurple()
 {
     SetConsoleTextAttribute(h,0x05 | 0xf8);
 }
-void darkyellow()
+inline void darkyellow()
 {
     SetConsoleTextAttribute(h,0x06 | 0xf8);
 }
-void darkwhite()
+inline void darkwhite()
 {
     SetConsoleTextAttribute(h,0x07 | 0xf8);
 }
-void gray()
+inline void gray()
 {
     SetConsoleTextAttribute(h,0x08 | 0xf8);
 }
-void blue()
+inline void blue()
 {
     SetConsoleTextAttribute(h,0x09 | 0xf8);
 }
-void green()
+inline void green()
 {
     SetConsoleTextAttribute(h,0x0a | 0xf8);
 }
-void cyan()
+inline void cyan()
 {
     SetConsoleTextAttribute(h,0x0b | 0xf8);
 }
-void red()
+inline void red()
 {
     SetConsoleTextAttribute(h, 0x0c | 0xf8);
 }
-void purple()
+inline void purple()
 {
     SetConsoleTextAttribute(h,0x0d | 0xf8);
 }
-void yellow()
+inline void yellow()
 {
     SetConsoleTextAttribute(h,0x0e | 0xf8);
 }
-void white()
+inline void white()
 {
     SetConsoleTextAttribute(h,0x0f | 0xf8);
 }
